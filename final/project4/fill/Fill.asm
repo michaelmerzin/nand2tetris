7978839b4ca57f7371408703873c3b693c158f22
michaelmerzin <120664244+michaelmerzin@users.noreply.github.com>
2023-03-23 10:14:51 +0200

    Update Fill.asm

--- final/project4/fill/Fill.asm
+++ final/project4/fill/Fill.asm
@@ -1,7 +1,4 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+
 
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
@@ -11,7 +8,7 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
 
 (INF_LOOP)
 @KBD
